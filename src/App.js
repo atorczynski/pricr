dfad6ebb34c5fd37a532e748a99655d5bf302862
atorczynski <a.torczynski@gmail.com>
2020-07-02 22:15:51 +0200

    Add react-router and first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import GlobalStyles from './GlobalStyles';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Main from './Pages/Main';
+import { ContentContainer } from './Components/GlobalComponents/BodyHelper';
+import Header from './Components/Header/Header';
+
+import { ThemeProvider } from 'emotion-theming';
+import defaultTheme from './Themes/defaultThemes';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>HI</h1>
-    </div>
+    <Router>
+      <ThemeProvider theme={defaultTheme}>
+        <Header heading={'PRICR'} />
+        <GlobalStyles />
+        <ContentContainer>
+          <Route exact path={'/'}>
+            <Main />
+          </Route>
+        </ContentContainer>
+      </ThemeProvider>
+    </Router>
   );
 }
 
