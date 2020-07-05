0eb7e9330ed7e6b51e845ab21edec0b68004018e
atorczynski <a.torczynski@gmail.com>
2020-07-05 21:58:59 +0200

    Rework SearchBar component

--- src/Components/Container/SearchContainerComponents.js
+++ src/Components/Container/SearchContainerComponents.js
@@ -1,23 +1,25 @@
+import { Button } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import styled from '@emotion/styled';
 
 export const SearchContainerWrapper = styled.div`
-  background-image: url("https://i.imgur.com/KubB5Ps.jpg");
+  background-color: #f7eec7;
   width: 100%;
   height: 300px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  border-Bottom: 1px solid #F4D35E;
-  border-Top: 1px solid #F4D35E;
+  flex-direction: row;
+  justify-content: space-around;
+  border-bottom: 1px solid #f4d35e;
+  border-top: 1px solid #f4d35e;
+  box-shadow: 0px 6px 17px 1px rgba(0, 0, 0, 0.75);
 `;
 
 export const SearchBarWrapper = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-
 `;
 
 export const SearchBarTitle = styled.p`
@@ -31,42 +33,48 @@ export const SearchBar = styled.input`
   font-size: 16px;
   color: #555555;
   text-align: center;
-  border-radius: 5px;
-  border: 1px solid #DEDFDF;
+  border-radius: 5px 0 0 5px;
+  border: 1px solid #dedfdf;
 
   :focus {
     outline: none;
     text-align: left;
     padding-left: 10px;
-    border: 1px solid #EE964B;
+    border: 1px solid #ee964b;
     transition: border 1s ease-in;
   }
 `;
 
-export const FindButton = styled.button`
-  background: #EE964B;
-  color: #FFF;
+export const SearchBarButtonWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+`;
+
+export const SearchButton = styled(Button)`
   outline: none;
-  border-radius: 5px;
-  box-shadow: 0px -7px #FFF;
-  border: 2px solid #FFF;
+  width: 50px;
+  height: 38px;
+  background-color: black;
   cursor: pointer;
   font-size: 17px;
   font-weight: 400;
-  margin-top: 15px;
   padding: 7px 15px;
   text-transform: uppercase;
+  border-radius: 0px;
 
   :hover {
-    background: #F4D35E;
-
+    background: #f4d35e;
   }
-
-  :active {
-    background: #F4D35E;
-    box-shadow: 0px -3px #FFF;
-    transform: translateY(-4px);
-    
-   
-  }
-`
+`;
